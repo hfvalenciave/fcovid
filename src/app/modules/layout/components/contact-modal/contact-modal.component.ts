319193cfe2ce27f91d8dec33f9fe06d9f1136a81
Carlos <ceaf.21@gmail.com>
2020-04-17 11:34:30 -0400

    Add contact service

--- src/app/modules/layout/components/contact-modal/contact-modal.component.ts
+++ src/app/modules/layout/components/contact-modal/contact-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import { ContactService } from 'src/app/modules/core/services/contact/contact.service';
+import { NotifierService } from "angular-notifier";
 
 @Component({
   selector: 'app-contact-modal',
@@ -13,16 +15,21 @@ export class ContactModalComponent {
   list: any[] = [];
   contactForm: FormGroup;
   modalRef: BsModalRef;
+  private readonly notifier: NotifierService;
  
-  constructor(public bsModalRef: BsModalRef, public fb: FormBuilder) {
-    this.contactForm = this.fb.group({
-      profession: [''],
-      comments: ['', [Validators.required]],
-      fullname: [''],
-      email: ['', [Validators.required]],
-      phone: [''],
-      timeZone: [''],
-    });
+  constructor(
+    public bsModalRef: BsModalRef, 
+    public fb: FormBuilder, 
+    public contactService: ContactService
+    ) {
+      this.contactForm = this.fb.group({
+        profession: [''],
+        comments: ['', [Validators.required]],
+        fullname: [''],
+        email: ['', [Validators.email]],
+        phone: [''],
+        timeZone: [''],
+      });
   }
 
   hideModal() {
@@ -34,7 +41,11 @@ export class ContactModalComponent {
   }
 
   onSubmit() {
-      console.log('form submitted');
+    if(this.contactForm.valid) {
+      this.contactService.sendEmail(this.contactForm.value, 'anonymous');
+      this.hideModal();
+
+    } 
   }
 
 
