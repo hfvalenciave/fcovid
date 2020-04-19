5a7ac83b067d6e01f436ca9ae2934811864831f5
bpena <bernardo.pena.ramos@gmail.com>
2020-04-19 17:10:12 -0400

    ajustando la url del api en producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api: {
-    url: 'http://localhost:3000/fcovid/api/v1'
+    url: 'http://wf-207-38-92-44.webfaction.com:10223/fcovid/api/v1'
   }
 };
