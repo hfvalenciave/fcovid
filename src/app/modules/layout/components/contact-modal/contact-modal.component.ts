b172ddd1a7561ee22b1b042ee9280a04971aad6f
Carlos <ceaf.21@gmail.com>
2020-04-19 10:47:58 -0400

    delete notifier

--- src/app/modules/layout/components/contact-modal/contact-modal.component.ts
+++ src/app/modules/layout/components/contact-modal/contact-modal.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
 import { ContactService } from 'src/app/modules/core/services/contact/contact.service';
-import { NotifierService } from "angular-notifier";
 
 @Component({
   selector: 'app-contact-modal',
@@ -15,7 +14,6 @@ export class ContactModalComponent {
   list: any[] = [];
   contactForm: FormGroup;
   modalRef: BsModalRef;
-  private readonly notifier: NotifierService;
  
   constructor(
     public bsModalRef: BsModalRef, 
